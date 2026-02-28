bd063b7a05a63e60ded95471fec0f5438bd13ee3
kkandregula-AI <kkandregula@gmail.com>
2026-02-28 17:12:37 +0530

    DriveNav PWA

---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open("drive-nav-v1").then(cache=>{
+      return cache.addAll(["./","./index.html","./manifest.webmanifest"]);
+    })
+  );
+});
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(res=>res||fetch(e.request))
+  );
+});
